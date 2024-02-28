f35fa2e4933184f3cad82c4dd1b555e5a6a7fb0e
developervicky <developer.vicky6@gmail.com>
2024-02-28 23:57:45 +0530

    refactor: stripe webhook

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -21,7 +21,6 @@ const EventPage = async ({
     page: searchParams.page as string,
   });
 
-  console.log(searchParams.page as string);
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,14 +1,21 @@
-import stripe from "stripe";
-import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import { NextResponse, NextRequest } from "next/server";
 import { createOrder } from "@/lib/actions/order.actions";
+import { headers } from "next/headers";
 
-export async function POST(request: Request) {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16",
+
+  typescript: true,
+});
+
+export async function POST(request: NextRequest) {
   const body = await request.text();
 
-  const sig = request.headers.get("stripe-signature") as string;
+  const sig = headers().get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-  let event;
+  let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
