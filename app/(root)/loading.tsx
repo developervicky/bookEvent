aa9c863cf9b9ed7115ab6d2a3fed0d879516d66c
developervicky <developer.vicky6@gmail.com>
2024-02-29 16:30:55 +0530

    feat: custom scrollbar, loading page

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { InfinitySpin } from "react-loader-spinner";
+
+const Loading = () => {
+  return (
+    <section className="h-full flex justify-center items-center py-5 md:py-10">
+      <div className="wrapper mx-10 bg-grey-50 rounded-2xl flex flex-col gap-4 flex-center">
+        <h3 className="h3-bold text-center">
+          Your Page is loading, spare some seconds!
+        </h3>
+        <InfinitySpin
+          visible={true}
+          width="200"
+          color="#6D4C3D"
+          ariaLabel="infinity-spin-loading"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
